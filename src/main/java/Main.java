f075b905fb11abf09a18c465fce517ee3ad9aa8c
Yaoxing <yaoxing.zhang@gmail.com>
2018-06-12 19:38:40 +0800

    + 添加了中文注释

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,34 +11,39 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 public class Main {
     public static void main(String[] args) {
         System.out.println("====This is a MongoDB demo!====");
+
+        // MongoDB连接字符串
         ConnectionString connStr = new ConnectionString("mongodb://127.0.0.1/demo");
 
+        // 用于转换POJO与BSON的转换器。如果不使用POJO则不需要调用
         CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(
                 com.mongodb.MongoClient.getDefaultCodecRegistry(),
                 CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
+        // 使用连接字符串，POJO转换器生成一个MongoDB配置
         MongoClientSettings settings = MongoClientSettings.builder()
                 .codecRegistry(pojoCodecRegistry)
                 .applyConnectionString(connStr)
                 .build();
+
         MongoClient client = MongoClients.create(settings);
 
-        // Cleanup and generate sample data x10000
+        // 清理测试集合并生成测试数据10000条
         Insert gen = new Insert(client);
         gen.cleanup();
         gen.insertData();
 
-        // Update array element
+        // 更新数组元素
         Update update = new Update(client);
         update.updateSingleElm();
         update.updateAllMatched();
 
-        // Run aggregation
-        Aggregate agg = new Aggregate(client);
-        agg.AggregateByFavouriteColor();
-
-        // Run array query
+        // 查询数组元素
         Find f = new Find(client);
         f.FindSingleArrayElm();
+
+        // 执行聚合
+        Aggregate agg = new Aggregate(client);
+        agg.AggregateByFavouriteColor();
     }
 }
